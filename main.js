ef8a18152c8162874a017d15a78e0b56420a6c50
syukyu8 <s153097@matsu.shimane-u.ac.jp>
2021-07-01 19:21:00 +0900

    Prepared items for umamusume data entry

---
+++ main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        race: 'チームレース履歴登録',
+        uma:[
+            {
+                name: 'ウマ娘',
+                speed: 'スピード',
+                stamina: 'スタミナ',
+                power: 'パワー',
+                guts: '根性',
+                wise: '賢さ',
+                ranking: '順位'
+            }
+        ]
+    }
+})
