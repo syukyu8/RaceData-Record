a4ffcc51a197c67922c70ecb75cfd7b7b93c041a
syukyu8 <s153097@matsu.shimane-u.ac.jp>
2021-07-02 20:32:11 +0900

    7/2 struggling with an error that won't read inputdata

--- main.js
+++ main.js
@@ -15,3 +15,42 @@ var app = new Vue({
         ]
     }
 })
+
+var inputdata = new Vue({
+    el: '#inputdata',
+    data() {
+        return {
+            errors: [],
+            name: null,
+            speed: null,
+            stamina: null,
+            power: null,
+            guts: null,
+            wise: null,
+            ranking: null
+        }
+  },
+    methods:{
+        onsubmit() {
+            if (this.name && this.speed && stamina && power && guts && wise && ranking) {
+                this.name = null
+                this.speed = null
+                this.stamina = null
+                this.power = null
+                this.guts = null
+                this.wise = null
+                this.ranking = null
+            }
+            else{
+                if (!this.name) this.errors.push('name required.')
+                if (!this.speed) this.errors.push('speed required.')
+                if (!this.stamina) this.errors.push('stamina required.')
+                if (!this.power) this.errors.push('power required.')
+                if (!this.guts) this.errors.push('guts required.')
+                if (!this.wise) this.errors.push('wise required.')
+                if (!this.ranking) this.errors.push('ranking required.')
+                
+            }
+        }
+    }
+})
